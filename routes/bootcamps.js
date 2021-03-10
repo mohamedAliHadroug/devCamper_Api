4a5b02a019598fb54517379a4aa128a3e79ab205
mohamedAliHadroug <hadrougmh@gmail.com>
2021-03-10 19:30:19 +0100

    course model

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,11 +6,14 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius
 } = require("../controllers/bootcamps");
 router.route("/").get(getBootcamps);
 router.route("/:id").get(getBootcamp);
 router.route("/").post(createBootcamp);
 router.route("/:id").put(updateBootcamp);
 router.route("/:id").delete(deleteBootcamp);
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
 
 module.exports = router;
