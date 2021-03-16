b9b25f86f37a7b45d096f85d523b7b9061110d52
mohamedAliHadroug <hadrougmh@gmail.com>
2021-03-16 18:35:03 +0100

    reset password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -38,7 +39,7 @@ const UserSchema = new mongoose.Schema({
   // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    next(); 
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -56,5 +57,37 @@ UserSchema.methods.getSignedJwtToken = function () {
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+// Generate email confirm token
+// UserSchema.methods.generateEmailConfirmToken = function (next) {
+//   // email confirmation token
+//   const confirmationToken = crypto.randomBytes(20).toString('hex');
+
+//   this.confirmEmailToken = crypto
+//     .createHash('sha256')
+//     .update(confirmationToken)
+//     .digest('hex');
+
+//   const confirmTokenExtend = crypto.randomBytes(100).toString('hex');
+//   const confirmTokenCombined = `${confirmationToken}.${confirmTokenExtend}`;
+//   return confirmTokenCombined;
+// };
 
   module.exports = mongoose.model('User', UserSchema);
